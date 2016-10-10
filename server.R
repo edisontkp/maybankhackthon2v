ada986147c7263b0624da9c53943c6975fbf6fd9
edisontkp <edison88tkp@gmail.com>
2016-10-10 18:31:17 -0400

    update

--- server.R
+++ server.R
@@ -1,1 +1,14 @@
-libary("shiny")
+library(shiny)
+
+data_file<-read.table("http://www.fabioveronesi.net/ShinyApp/point_grid.csv",sep=";",header=T) 
+
+shinyServer(function(input,output){
+  
+  output$plot <- renderPlot({
+    sub<-data_file[input$row,]
+    data<-rnorm(10000,mean=sub$Wind_Media,sd=sub$StDev_smoo)
+    hist(data)
+  })
+  
+  
+})
